7704b9f2c3d8d227d114924a40ee619ebb8aa28e
Brenda Valadares <brenda.valadares@trindadetecnologia.com.br>
2025-04-15 12:37:27 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    private readonly static List<Movie> movies = new();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+       movies.Add(movie);
+
+       Console.WriteLine($"Movie added: {movie.Title}");
+       Console.WriteLine($"Duration: {movie.Duration}");
+    }
+}

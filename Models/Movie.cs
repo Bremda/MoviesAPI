a7c8e1e1ba947595c6a6ba2db8434bc5f3916b04
Brenda Valadares <brenda.valadares@trindadetecnologia.com.br>
2025-04-30 19:17:22 -0300

    Add validation attributes

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,9 +1,19 @@
-﻿namespace MoviesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
 {
     public class Movie
     {
+        [Required(ErrorMessage = "The movie title is required")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "The movie genre is required")]
+        [MaxLength(50, ErrorMessage = "The genre length must not exceed 50 characters")]
         public string Genre { get; set; }
+
+
+        [Required]
+        [Range(70, 600, ErrorMessage = "The duration must be between 70 and 600 minutes")]
         public int Duration { get; set; }
     }
 }
